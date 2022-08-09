c57dbec5ebf92291662690e86cd1e880dc28d50d
RoswellCR <rosbelcr@gmail.com>
2022-08-09 11:48:22 -0400

    constants

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import './styles.css';
+import {colorPrincipal} from './colors'
 
-// components
-import Profile from './components/Profile';
-import UserList from './components/UserList';
+const borderGeneral = 'border-radius:0.9em;'
 
-//CONTEXT
-import UserState from './context-user/User/UserState';
+const Header = styled.header 
+//background: var(--colorPrincipal);
+`background:${colorPrincipal};
+text-align: center;
+padding:0.3em;
+margin:0.3em;
+${borderGeneral}
+` 
 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+const Subtitle = styled.h1`
+color: ${colorPrincipal};
+`
 
 function App() {
   return (
     
-        <div className='container p-4'>
-          <h1>Hola Mundo<h/>
+        <div>
+          <Header>
+          
+           <h1>Styled component</h1>
+
+          </Header>
+          <Subtitle>
+            Title example
+          </Subtitle>
         </div>
     
   );
