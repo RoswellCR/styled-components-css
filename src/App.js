19bdd29d5c732caa0c5ab1a78d9bd7142a157bc7
RoswellCR <rosbelcr@gmail.com>
2022-08-09 12:28:01 -0400

    dinamic vars with functions

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import styled from 'styled-components';
 import './styles.css';
 import {colorPrincipal} from './colors'
 
+//variables completas
 const borderGeneral = 'border-radius:0.9em;'
+
+//variables dinamicas con funciones
+const getLinearGradient = (rot)=>{
+  return `background:linear-gradient(${rot},#db7093,#daa357);`
+}
 
 const Header = styled.header 
 //background: var(--colorPrincipal);
 `background:${colorPrincipal};
+${getLinearGradient('50deg')}
 text-align: center;
 padding:0.3em;
 margin:0.3em;
