2c11aeec7f57d411a99fd7272dc60803a7823988
RoswellCR <rosbelcr@gmail.com>
2022-08-10 00:32:15 -0400

    animation 2

--- src/App.js
+++ src/App.js
@@ -4,24 +4,39 @@ import './styles.css';
 import {colorPrincipal} from './colors'
 
 
-const Anima = keyframes`  
-from {
-  background: #000;
-}
-to {
-  background: #db7093;
-}
-`
+
 
 const Header = styled.header 
-//background: var(--colorPrincipal);
 `background:${colorPrincipal};
 text-align: center;
 padding:0.3em;
 margin:0.3em;
-animation: ${Anima} 2ms ease-in-out infinite;
+`
+const pulse = keyframes`
+0% {
+  transform: scale(1);
+  background: gray;
+  color: #000;
+}
+50% {
+  transform: scale(1.3);
+  background: purple;
+  color: #FFF;
+}
+100% {
+  transform: scale(1);
+  background: gray;
+  color: #000;
+}
 `
 
+const Button = styled.button`
+margin: 1em 2em;
+padding: 1em;
+&:hover{
+  animation: ${pulse} 2s ease-in-out;
+}
+`
 
 function App() {
   return (
@@ -34,6 +49,8 @@ function App() {
             
           </Header>
           
+          <Button>Button</Button>
+
         </div>
     
   );
